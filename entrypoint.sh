42af38f002e5fd312d327e6a7cf7d7a631eee775
Ash Liu <root@viper.run>
2025-06-12 17:41:59 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 echo "Starting Caddy with env:"
 env | grep -E 'DOMAIN|PORT|FRONTEND|BACKEND'
 
-exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+exec caddy run --config /app/Caddyfile --adapter caddyfile
